b0ce60c4febca979408d61eec89120ed0771363a
AsTech-900 <sallu@gmail.com>
2025-02-08 18:59:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#Stop-Watch
+
+Project Overview : 
+This is a simple stopwatch web application built using HTML, CSS, and JavaScript. The stopwatch allows users to start, pause, and reset the timer with a sleek UI and animated border effects.
+
+# Features 
+
+* Start, Pause, and Reset functionality
+* Smooth UI Design with a glowing animated border
+* Accurate time tracking (hours, minutes, and seconds)
+* Responsive Design
+
+Technologies Used 
+
+* HTML 
+* CSS 
+* JavaScripts
+
+
+Usages
+* Click the Play button to start the stopwatch.
+* Click the Pause button to stop the timer.
+* Click the Reset button to reset the stopwatch to 00:00:00.
---
+++ app.js
@@ -0,0 +1,65 @@
+let hourEl = document.getElementById("hour");
+let minusEl = document.getElementById("minut");
+let secondEl = document.getElementById("second");
+
+let playBtn = document.getElementById('play-btn');
+let pauseBtn = document.getElementById('pause-btn');
+let resetBtn = document.getElementById('reset-btn');
+let timer = null;
+let [s,m,h] = [0,0,0];
+
+
+
+
+playBtn.addEventListener('click',()=>{
+   timer = setInterval(getTimes,1000);
+} )
+
+pauseBtn.addEventListener('click',()=>{
+    clearInterval(timer);
+})
+resetBtn.addEventListener('click',()=>{
+    resetWatch();
+})
+
+
+
+function resetWatch(){
+    clearInterval(timer);
+    s = 0;
+   m = 0;
+   h = 0;
+    secondEl.innerHTML = "00";
+    minusEl.innerHTML = "00:";
+    hourEl.innerHTML = "00:" 
+
+}
+
+
+function getTimes(){
+
+    s++;
+
+    s = (s < 10) ? "0" + s : s;
+
+    secondEl.innerHTML = s;
+    
+    if(s >= 59){
+        s = 0;
+        m++;
+        m = (m < 10) ? "0" + m : m;
+        minusEl.innerHTML = `${m}:`;
+        if(m >= 59){
+            m = 0;
+            h++;
+            h = (h < 10) ? "0" + h : h;
+            hourEl.innerHTML = `${h}:`;
+        }
+    }
+  
+  
+
+}
+
+
+
